2c092ecd02cb27e8333d1513644c996da7cbf54a
FRANCISDAX <149949315+FRANCISDAX@users.noreply.github.com>
2026-03-29 17:22:21 -0500

    Pais Controller

--- src/main/java/com/example/simulacro/simulacro_01/controller/PaisController.java
+++ src/main/java/com/example/simulacro/simulacro_01/controller/PaisController.java
@@ -4,25 +4,20 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.simulacro.simulacro_01.entity.Pais;
 import com.example.simulacro.simulacro_01.service.PaisService;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.extern.apachecommons.CommonsLog;
 
 @CommonsLog
 @RestController
 @RequestMapping("/api/paises")
+@Tag(name = "Paises", description = "Operaciones sobre Paises")
 public class PaisController {
 
     @Autowired
