41114a52f7fed127fb73341f012315ab58a3ebd7
FRANCISDAX <149949315+FRANCISDAX@users.noreply.github.com>
2026-03-29 15:50:05 -0500

    Mejoras Entidades

--- src/main/java/com/example/simulacro/simulacro_01/entity/Pais.java
+++ src/main/java/com/example/simulacro/simulacro_01/entity/Pais.java
@@ -2,12 +2,14 @@ package com.example.simulacro.simulacro_01.entity;
 
 import java.io.Serializable;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Schema(description = "Entidad que representa un país")
 @Entity
 @Table(name="pais")
 @Getter @Setter
@@ -16,8 +18,13 @@ public class Pais implements Serializable{
 
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
+     @Schema(description = "ID País", example = "173")
     private Long idPais;
+
+    @Schema(description = "Abreviatura del País", example = "PE")
     private String iso;
+
+    @Schema(description = "Nombre del País", example = "Perú")
     private String nombre;
 
 }
--- src/main/java/com/example/simulacro/simulacro_01/entity/Tipo.java
+++ src/main/java/com/example/simulacro/simulacro_01/entity/Tipo.java
@@ -1,11 +1,13 @@
 package com.example.simulacro.simulacro_01.entity;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Schema(description = "Entidad que representa un tipo de proveedor")
 @Entity
 @Table(name="tipo")
 @Getter @Setter
@@ -14,7 +16,10 @@ public class Tipo {
 
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Schema(description = "ID tipo de proveedor", example = "3")
     private Long idTipo;
+
+    @Schema(description = "Descripción tipo de proveedor", example = "Local")
     private String descripcion;
 
 }
